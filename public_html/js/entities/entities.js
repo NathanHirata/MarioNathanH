680be964755bfe5a3f3bf3676f5380eac9fb5309
Nathan Hirata <nathanhirata@yahoo.com>
2014-11-30 23:35:09 -0800

    vid 14

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -8,7 +8,7 @@ game.PlayerEntity = me.Entity.extend({
                 width: 128,
                 height: 128,
                 getShape: function() {
-                    return(new me.Rect(0, 0, 128, 128)).toPolygon();
+                    return(new me.Rect(0, 0, 30, 128)).toPolygon();
                 }
         }]);
     
@@ -18,6 +18,7 @@ game.PlayerEntity = me.Entity.extend({
         
         
         this.body.setVelocity(5, 20);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     
     update: function(delta) {
@@ -60,11 +61,13 @@ game.LevelTrigger = me.Entity.extend({
         this._super(me.Entity, 'init', [x, y, settings]);
         this.body.onCollision = this.onCollision.bind(this);
         this.level = settings.level;
+        this.xSpawn = settings.xSpawn;
+        this.ySpawn = settings.ySpawn;
     },
     
     onCollision: function() {
         this.body.setCollisionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
-        me.state.current
+        me.state.current().resetPlayer(this.xSpawn, this.ySpawn);
     }
 });
